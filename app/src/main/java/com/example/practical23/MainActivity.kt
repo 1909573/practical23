0a96a1c0c3a903cbcf4e3a3783761fff6d24472a
taruc <cshong199@hotmail.com>
2019-10-30 14:48:15 +0800

    232

---
+++ app/src/main/java/com/example/practical23/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.practical23
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for the background
+            R.id.box1 -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box2 -> view.setBackgroundColor(Color.GRAY)
+            R.id.box3 -> view.setBackgroundColor(Color.BLUE)
+            R.id.box4 -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box5 -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.BLACK)
+        }
+    }
+
+    private fun setListeners() {
+        val redButton: Button = findViewById(R.id.red)
+        val blueButton: Button = findViewById(R.id.blue)
+        val greenButton: Button = findViewById(R.id.green)
+
+        val boxOneText = findViewById<TextView>(R.id.box1)
+        val boxTwoText = findViewById<TextView>(R.id.box2)
+        val boxThreeText = findViewById<TextView>(R.id.box3)
+        val boxFourText = findViewById<TextView>(R.id.box4)
+        val boxFiveText = findViewById<TextView>(R.id.box5)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraintLayout)
+
+        val clickableViews: List<View> =
+            listOf(
+                boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+}
